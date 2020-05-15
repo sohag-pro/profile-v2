cf495273f0683bc6d791b9ece302e797e28dd93d
shuvo575 <shohaghasan575@gmail.com>
2020-05-15 11:56:34 +0600

    Animation added

--- js/init.js
+++ js/init.js
@@ -3,5 +3,10 @@
 
     $('.collapsible').collapsible();
 
+    setTimeout(function() {
+      //your code to be executed after 1 second
+      M.toast({html: 'Click on title for more info.'})
+    }, 5000);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
